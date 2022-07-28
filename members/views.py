7bee6deefd445e346000ab93da8c1dc1bb87b5c1
Iehana's Ideapad <NoranekoHeart@outlook.jp>
2022-07-28 15:23:35 +0800

    data touroku

--- members/views.py
+++ members/views.py
@@ -1,6 +1,34 @@
-from django.http import HttpResponse
+from re import template
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
+from .models import Members
+
+#defines index.html
 def index(request):
-  template = loader.get_template('myfirst.html')
-  return HttpResponse(template.render())
+  mymembers = Members.objects.all().values()
+  template = loader.get_template('index.html')
+  context = {
+    # 辞書のキーはindex.htmlで使う変数
+    'mymembers' : mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+
+# defines add.html
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+
+  # リクエスト元のデータを取得
+  x = request.POST['first']
+  y = request.POST['last']
+
+  # dbに登録
+  member = Members(firstname=x, lastname=y)
+  member.save()
+
+  # 別のhtmlを作らず、既存のindex.htmlへ移行
+  return HttpResponseRedirect(reverse('index'))
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Django Tutorial Example
+from w3schools
+https://www.w3schools.com/django/index.php
